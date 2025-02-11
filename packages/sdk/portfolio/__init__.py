ef67569a5c44f059bfa22d46eeb74b46148fdafd
Guillem <guillemavila@gmail.com>
2025-02-11 20:30:27 +0100

    add external_position_manager

--- packages/sdk/portfolio/__init__.py
+++ packages/sdk/portfolio/__init__.py
@@ -1,5 +1,24 @@
 from . import asset_managers
 from . import integrations
+from .._internal.external_position_manager import (
+    ACTION as EXTERNAL_POSITION_ACTION,
+    call as call_external_position,
+    call_encode as call_external_position_encode,
+    call_decode as call_external_position_decode,
+    CallArgs as ExternalPositionCallArgs,
+    create as create_external_position,
+    create_encode as create_external_position_encode,
+    create_decode as create_external_position_decode,
+    CreateArgs as CreateExternalPositionArgs,
+    remove as remove_external_position,
+    remove_encode as remove_external_position_encode,
+    remove_decode as remove_external_position_decode,
+    RemoveArgs as RemoveExternalPositionArgs,
+    reactivate as reactivate_external_position,
+    reactivate_encode as reactivate_external_position_encode,
+    reactivate_decode as reactivate_external_position_decode,
+    ReactivateArgs as ReactivateExternalPositionArgs,
+)
 from .._internal.integration_manager import (
     ACTION as INTEGRATION_ADAPTER_ACTION,
     SELECTOR as INTEGRATION_ADAPTER_SELECTOR,
@@ -19,6 +38,29 @@ __all__ = [
     # Submodules
     "asset_managers",
     "integrations",
+
+
+    # External position manager functions
+    "call_external_position",
+    "call_external_position_encode",
+    "call_external_position_decode",
+    "create_external_position",
+    "create_external_position_encode",
+    "create_external_position_decode",
+    "remove_external_position",
+    "remove_external_position_encode",
+    "remove_external_position_decode",
+    "reactivate_external_position",
+    "reactivate_external_position_encode",
+    "reactivate_external_position_decode",
+
+    # Constants and types
+    "EXTERNAL_POSITION_ACTION",
+    "ExternalPositionCallArgs",
+    "CreateExternalPositionArgs",
+    "RemoveExternalPositionArgs",
+    "ReactivateExternalPositionArgs",
+
 
     # Integration manager functions
     "call_integration_adapter",
