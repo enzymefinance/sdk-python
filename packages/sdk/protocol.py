3eb80ab626efc2f7daf173fffd7d4a334649252d
Guillem <guillemavila@gmail.com>
2025-03-05 18:56:04 +0100

    add protocol

---
+++ packages/sdk/protocol.py
@@ -0,0 +1,80 @@
+from web3.types import ChecksumAddress
+from eth_abi import encode
+from .utils.clients import PublicClient
+from .utils.encoding import encoding_to_types
+
+
+async def is_supported_asset(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> bool:
+    contract = client.contract(
+        value_interpreter,
+        "IValueInterpreter",
+    )
+    function = contract.functions.isSupportedAsset(asset)
+    return await function.call()
+
+
+async def is_supported_asset_for_derivative_price_feed(
+    client: PublicClient,
+    derivative_price_feed: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> bool:
+    contract = client.contract(
+        derivative_price_feed,
+        "IDerivativePriceFeed",
+    )
+    function = contract.functions.isSupportedAsset(asset)
+    return await function.call()
+
+
+async def is_supported_primitive_asset(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> bool:
+    contract = client.contract(value_interpreter, "IValueInterpreter")
+    function = contract.functions.isSupportedPrimitiveAsset(asset)
+    return await function.call()
+
+
+async def is_supported_derivative_asset(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> bool:
+    contract = client.contract(value_interpreter, "IValueInterpreter")
+    function = contract.functions.isSupportedDerivativeAsset(asset)
+    return await function.call()
+
+
+async def get_aggregator_for_primitive(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> ChecksumAddress:
+    contract = client.contract(value_interpreter, "IValueInterpreter")
+    function = contract.functions.getAggregatorForPrimitive(asset)
+    return await function.call()
+
+
+async def get_rate_asset_for_primitive(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> ChecksumAddress:
+    contract = client.contract(value_interpreter, "IValueInterpreter")
+    function = contract.functions.getRateAssetForPrimitive(asset)
+    return await function.call()
+
+
+async def get_price_feed_for_derivative(
+    client: PublicClient,
+    value_interpreter: ChecksumAddress,
+    asset: ChecksumAddress,
+) -> ChecksumAddress:
+    contract = client.contract(value_interpreter, "IValueInterpreter")
+    function = contract.functions.getPriceFeedForDerivative(asset)
+    return await function.call()
